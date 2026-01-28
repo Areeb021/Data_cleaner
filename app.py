70b13732a1769f907864d1d16d6cff57c2b90a70
Areeb Bhatti <bhattiareeb14@gmail.com>
2026-01-28 11:32:40 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ if uploaded_file:
     if uploaded_file.name.endswith(".csv"):
         df = pd.read_csv(uploaded_file)
     else:
-        df = pd.read_excel(uploaded_file, engine="openpyxl")
+        df = pd.read_excel(uploaded_file)
 
 
     st.subheader("🔍 Raw Data Preview")
